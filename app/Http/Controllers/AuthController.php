b8aa26d8feac582a08c5cd4799580a7bd040743c
vikarmaulanaarrisyad <vikar.maulana.arrisyad@gamil.com>
2022-07-24 15:10:22 +0700

    menambahkan fitur login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        try {
+                    if (Auth::attempt($request->only('email','password'))) {
+            $user   = Auth::user();
+            $token  = $user->createToken('app')->accessToken;
+
+            return response([
+                'message'   =>  'SuccessFully Login',
+                'token'     =>  $token,
+                'user'      =>  $user
+            ], 400);
+        }
+        } catch (\Throwable $th) {
+            return response([
+                'message'   => $th->getMessage()
+            ],400);
+        }
+        
+        return response([
+            'message'   =>  'Invalid Email Or Password'
+        ],401);
+    }
 }
